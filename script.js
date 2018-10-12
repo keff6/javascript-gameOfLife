83d120e5e4300777e93072f56d4e211750cbcbf8
keff6 <kev.fallas@gmail.com>
2018-10-12 16:58:32 -0600

    Code refactor - Module revealed pattern

--- script.js
+++ script.js
@@ -1,27 +1,73 @@
-var App = (function (document) {
-  // Global variables
-  let heightValueUI = document.getElementById('input-height');
-  let widthValueUI = document.getElementById('input-width');
-  let speedInfo = document.getElementById('speed-info');
-  let heightVal = 0;
-  let widthVal = 0;
-  let gridValues = [];
-  let play;
-  const speedControl = document.getElementById('speed-control');  
-  const speed = {
-    0: { ms: 2000, info: '-2x'}, 
-    1: { ms: 1500, info: '-1.5x'}, 
-    2: { ms: 1000, info: '1x'}, 
-    3: { ms: 500, info: '1.5x'}, 
-    4: { ms: 250, info: '1.75x'}
-  };
-  let currentSpeed = 1000;
+/*---------------------------------------------------------------------------------------
+                                        UIController     
+/*---------------------------------------------------------------------------------------*/
+const UI = ((document) => {
 
-  // const canvas = document.getElementById('canvas');
-  const btnGenerate = document.getElementById('btn-generate');
-  const btnClear = document.getElementById('btn-clear');
-  const btnPlay = document.getElementById('btn-play');
-  const btnStep = document.getElementById('btn-step');
+  const table = document.getElementById('table'),
+        nodes = document.getElementsByTagName('td'),
+        variables = {
+          heightValue: document.getElementById('input-height').value,
+          widthValue: document.getElementById('input-width').value,
+          speedInfo: document.getElementById('speed-info'),
+          speedControl: document.getElementById('speed-control'),
+          btnGenerate: document.getElementById('btn-generate'),
+          btnClear: document.getElementById('btn-clear'),
+          btnPlay: document.getElementById('btn-play'),
+          btnStep: document.getElementById('btn-step'),
+        }
+
+  function setCellEvents(gridValues) {    
+    const spots = Array.prototype.slice.call(nodes,0);
+    spots.forEach(spot => spot.addEventListener('click', (event) => {
+      const isBlank = spot.getElementsByClassName("blank")[0] ? true : false;
+      const positionI = event.path[1].dataset.i;      
+      const positionJ = event.path[1].dataset.j;      
+      let d = '';
+      
+      if(positionI && positionJ) {
+        if(isBlank) {
+          d = spot.getElementsByClassName("blank")[0];
+          d.classList.remove('blank');
+          d.classList.add('dot');
+          
+          gridValues[positionI][positionJ] = 1;
+        }else {
+          d = spot.getElementsByClassName("dot")[0];
+          d.classList.remove('dot');
+          d.classList.add('blank');
+          gridValues[positionI][positionJ] = 0;
+        }
+      }      
+    }));
+  } 
+
+  function drawGrid(height, width, gridValues) {
+    let gridHtml = '';
+
+    for (let i = 0; i < height; i++) {
+      let row = gridValues[i];
+      gridHtml += '<tr>'
+      for (let j = 0; j < width; j++) {
+        gridHtml += `<td data-i="${i}" data-j="${j}"><div class="${row[j] === 1 ? 'dot' : 'blank'}"></div></td>`
+      }
+      gridHtml += '</tr>'
+    }
+
+    table.innerHTML = gridHtml;
+    setCellEvents(gridValues);   
+  }
+
+  return {
+    variables: variables,
+    drawGrid: drawGrid
+  }
+
+})(document);
+/*---------------------------------------------------------------------------------------
+                                        LogicController     
+/*---------------------------------------------------------------------------------------*/
+const Controller = ((UI) => {
+  let gridValues = [];
 
   function generateRandomBit() {
     return Math.round(Math.random())
@@ -86,95 +132,84 @@ var App = (function (document) {
       nextGen[i] = newRow;
     }
     gridValues = nextGen;    
-    paintGrill();
+    return gridValues;
+  }
+
+  function getClearGrid(height, width) {
+    gridValues = generateClearGrid(height, width);
+    return gridValues;
+  }
+
+  function generateInitialGen(height, width){    
+    gridValues = generateValuesGrid(height, width);
+    return gridValues;      
+  }
+
+  return {
+    generateInitialGen: generateInitialGen,
+    getClearGrid: getClearGrid,
+    getNextGeneration: getNextGeneration,
+  }
+
+})(UI);
+
+
+/*---------------------------------------------------------------------------------------
+                                        AppController     
+/*---------------------------------------------------------------------------------------*/
+
+const App = ((UI, Controller) => { 
+  const speed = {
+    0: { ms: 2000, info: '-2x'}, 
+    1: { ms: 1500, info: '-1.5x'}, 
+    2: { ms: 1000, info: '1x'}, 
+    3: { ms: 500, info: '1.5x'}, 
+    4: { ms: 250, info: '1.75x'}
+  };
+  const height = UI.variables.heightValue || 50,
+        width = UI.variables.widthValue || 70;
+  let play;
+  let currentSpeed = 1000;
+
+  function generate() {
+    const gridValues = Controller.generateInitialGen(height, width);
+    UI.drawGrid(height, width, gridValues);
+  }
+
+  function clear() {
+    clearInterval(play);
+    const gridValues = Controller.getClearGrid(height, width);
+    UI.drawGrid(height, width, gridValues);
   }
 
   function start() {
-    play = setInterval(getNextGeneration, currentSpeed);
+    play = setInterval(
+      () => UI.drawGrid(height, width, Controller.getNextGeneration()), 
+      currentSpeed
+    );
   }
 
   function step() {
-    clearInterval(play)
-    getNextGeneration(); 
+    clearInterval(play);
+    UI.drawGrid(height, width, Controller.getNextGeneration());
   }
 
-  function getClearGrid() {
-    clearInterval(play)
-    heightVal = heightValueUI.value || 5;
-    widthVal = widthValueUI.value || 5;
-
-    gridValues = generateClearGrid(heightVal,widthVal);
-    paintGrill(); 
+  function setSpeed(value) {
+    clearInterval(play);
+    currentSpeed = speed[+value].ms;
+    UI.variables.speedInfo.innerHTML = speed[+value].info;
+    play = setInterval(
+      () => UI.drawGrid(height, width, Controller.getNextGeneration()), 
+      currentSpeed
+    );
   }
-
-  function generateInitialGen(){
-    clearInterval(play)
-    heightVal = heightValueUI.value || 5;
-    widthVal = widthValueUI.value || 5;
-
-    gridValues = generateValuesGrid(heightVal,widthVal);
-    paintGrill(); 
-  }
-
-  function paintGrill() {
-    let gridHtml = '';
-
-    for (let i = 0; i < heightVal; i++) {
-      let row = gridValues[i];
-      gridHtml += '<tr>'
-      for (let j = 0; j < widthVal; j++) {
-        gridHtml += `<td data-i="${i}" data-j="${j}"><div class="${row[j] === 1 ? 'dot' : 'blank'}"></div></td>`
-      }
-      gridHtml += '</tr>'
-    }
-
-    const table = document.getElementById('table');
-    table.innerHTML = gridHtml;
-    setCellEvents()
-  }
-
-  function outputUpdate(vol) {
-    clearInterval(play)
-    console.log(speed, vol)
-    currentSpeed = speed[+vol].ms;
-    speedInfo.innerHTML = speed[+vol].info;
-    console.log(currentSpeed)
-    play = setInterval(getNextGeneration, currentSpeed);
-  }
-
-  function setCellEvents() {
-    const tdNodes = document.getElementsByTagName('td');
-    const spots = Array.prototype.slice.call(tdNodes,0);
-    spots.forEach(spot => spot.addEventListener('click', (event) => {
-      console.log(spot)
-      const isBlank = spot.getElementsByClassName("blank")[0] ? true : false;
-      const positionI = event.path[1].dataset.i;      
-      const positionJ = event.path[1].dataset.j;      
-      let d = '';
-      
-      if(positionI && positionJ) {
-        if(isBlank) {
-          d = spot.getElementsByClassName("blank")[0];
-          d.classList.remove('blank');
-          d.classList.add('dot');
-          
-          gridValues[positionI][positionJ] = 1;
-        }else {
-          d = spot.getElementsByClassName("dot")[0];
-          d.classList.remove('dot');
-          d.classList.add('blank');
-          gridValues[positionI][positionJ] = 0;
-        }
-      }      
-    }));
-  }  
   
   function setEventListeners() {
-    btnGenerate.addEventListener('click', generateInitialGen);
-    btnClear.addEventListener('click', getClearGrid);
-    btnPlay.addEventListener('click', start);    
-    btnStep.addEventListener('click', step);
-    speedControl.addEventListener('input', () => outputUpdate(event.target.value)); 
+    UI.variables.btnGenerate.addEventListener('click', generate);
+    UI.variables.btnClear.addEventListener('click', clear);
+    UI.variables.btnPlay.addEventListener('click', start);    
+    UI.variables.btnStep.addEventListener('click', step);
+    UI.variables.speedControl.addEventListener('input', () => setSpeed(event.target.value)); 
   }
 
   return {
@@ -184,6 +219,6 @@ var App = (function (document) {
     }
   }
 
-})(document);
+})(UI, Controller);
 
 App.init();
